0b468fa108566beb3db5cb65e34cc5a6cb7ea906
Billy Bell <billysroom@gmail.com>
2015-10-18 18:21:44 +0000

    Creating and inserting into sqlite table

--- main.c
+++ main.c
@@ -1,11 +1,71 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include "sqlite3.h"
+
+sqlite3* init_wifi_db()
+{
+    sqlite3 *db = NULL;
+
+    int rc = sqlite3_open("wifi.db", &db);
+
+    if (rc != SQLITE_OK)
+    {
+        printf("Cannot open wifi database: %s\n", sqlite3_errmsg(db));
+        sqlite3_close(db);
+    }
+    else
+    {
+        char *err_msg = NULL;
+        char *sql = "CREATE TABLE AccessPoints(id INTEGER AUTOINCREMENT, Name TEXT);";
+
+        rc = sqlite3_exec(db, sql, NULL, NULL, &err_msg);
+
+        if (rc != SQLITE_OK)
+        {
+            printf("Error creating AccessPoints table: %s\n", err_msg);
+            sqlite3_free(err_msg);
+            sqlite3_close(db);
+        }
+    }
+
+    return db;
+}
+
+bool insert_access_point(sqlite3* db)
+{
+    char *err_msg = NULL;
+    char *sql = "INSERT INTO AccessPoints VALUES ('NewAccessPoint')";
+
+    if (sqlite3_exec(db, sql, NULL, NULL, &err_msg) != SQLITE_OK)
+    {
+        printf("Error inserting into AccessPoints table: %s\n", err_msg);
+        sqlite3_free(err_msg);
+        return false;
+    }
+
+    return true;
+}
 
 int main()
 {
     printf("Starting main...\n");
 
-    printf("Using sqlite version: %s\n", sqlite3_libversion());    
+    sqlite3* wifiDb = init_wifi_db();
+
+    if (wifiDb == NULL)
+    {
+        return 1;
+    }
+
+    if (insert_access_point(wifiDb) == false)
+    {
+        return 1;
+    }
+
+    sqlite3_close(wifiDb);
+
+    int last_id = sqlite3_last_insert_rowid(wifiDb);
+    printf("Last inserted row id was %d\n", last_id);
 
     printf("Done.\n");
     return 0;
